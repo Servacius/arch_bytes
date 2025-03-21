689dcb4db88a2e69e6525eb9c1ac22e3eb53e1cc
Servacius <servacius.liber@gmail.com>
2025-03-21 18:36:46 +0700

    fix some typo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from src.api.v1.endpoints.arch_bytes_generator import router as arch_bytes_router
-from src.core.logging import settings
+from src.core.config import settings
 
 app = FastAPI(title="Software Architecture Information Generator", version="0.1.0")
-app.include_router(arch_bytes_router, prefix="/api/v1/")
+ 
+app.include_router(arch_bytes_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
